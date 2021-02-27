9b5afbadb8cf360efad412e87a1bbba9817247fe
Khoirul Asfian <asfian99@pop-os.localdomain>
2021-02-27 16:47:26 +0700

    fix bug strict

--- src/redux/cartSlice.ts
+++ src/redux/cartSlice.ts
@@ -1,5 +1,4 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { RootState } from "./store";
 import type { ProductItemType } from "../AppType";
 
 // Define a type for the slice state
