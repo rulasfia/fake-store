5151a4fca0ca588ab65d8df1e2cbf2e05fbbdeb7
M Khoirul Asfian <45413544+asfian99@users.noreply.github.com>
2021-10-10 14:11:42 +0700

    Update cartSlice.ts

--- src/redux/cartSlice.ts
+++ src/redux/cartSlice.ts
@@ -13,19 +13,8 @@ const initialState: CartState = {
 
 export const cartSlice = createSlice({
   name: "cart",
-  // `createSlice` will infer the state type from the `initialState` argument
   initialState,
   reducers: {
-    // increment: (state) => {
-    //   state.value += 1;
-    // },
-    // decrement: (state) => {
-    //   state.value -= 1;
-    // },
-    // Use the PayloadAction type to declare the contents of `action.payload`
-    incrementByAmount: (state, action: PayloadAction<number>) => {
-      // state.value += action.payload;
-    },
     addToCart: (state, action: PayloadAction<ProductItemType>) => {
       state.items?.push(action.payload);
     },
